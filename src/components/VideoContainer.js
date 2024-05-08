7da99897ead67ee2a53c280fb1cc94e3b93c4d8a
sakir456 <shaikhsakir1015@gmail.com>
2024-05-08 23:59:49 +0530

    watchPage

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -16,6 +16,8 @@ const getVideos = async () => {
 
 
     useEffect(() => {
+
+       
        getVideos()
     },[])
 
@@ -23,7 +25,7 @@ const getVideos = async () => {
     return (
         <div className="flex flex-wrap">
         {videos.map((video) => (
-     <Link to={"/watch?v"+ video.id}><VideoCard key={video.id} info={video} /></Link>
+     <Link key={video.id} to={"/watch?v="+ video.id}><VideoCard  info={video} /></Link>
         ))}
        
         </div>
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,21 +1,29 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { closeMenu } from '../utils/appSlice'
-import { useSearchParams } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import { closeMenu } from "../utils/appSlice";
 
 const WatchPage = () => {
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
 
-const [searchParams] = useSearchParams();
-console.log(searchParams);
-
-const dispatch = useDispatch()
-
-    useEffect(() => {
-       dispatch(closeMenu())
-    },[])
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
   return (
-    <div>WatchPage</div>
-  )
-}
+    <div className="px-5">
+      <iframe
+        width="900"
+        height="450"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
 
-export default WatchPage
+export default WatchPage;
