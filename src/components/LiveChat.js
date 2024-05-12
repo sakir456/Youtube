d39693736821e9ccd0a540834c9860e26f2c2c65
sakir456 <shaikhsakir1015@gmail.com>
2024-05-12 18:19:34 +0530

    chatStreamLive feature

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,13 +1,62 @@
+import { useEffect, useState } from "react"
 import ChatMessage from "./ChatMessage"
+import { useDispatch, useSelector } from "react-redux"
+import { addMessage } from "../utils/chatSlice";
+import { generateRandomName, makeRandomMessage } from "../utils/helper";
+
 
 
 const LiveChat = () => {
+
+  const [liveMessage, setLiveMessage] = useState("")
+  const dispatch = useDispatch();
+
+  const chatMessages = useSelector(store => store.chat.messages)
+
+   useEffect(() => {
+
+    const i = setInterval(() => {
+      console.log("API polling");
+      dispatch(addMessage({
+        name: generateRandomName(),
+        message: makeRandomMessage(30)
+      }))
+    }, 1500)
+
+   return () => clearInterval(i)
+   }, [])
+
+
   return (
-    <div className="  w-full h-[450px] p-2 border border-black bg-slate-100 rounded-lg">
-   <ChatMessage name="sakir shaikh"  message="This is live chat Feature for You Tube App😎" />
-  
-    </div>
-  )
-}
+    <>
+    <div className="  w-full h-[450px] p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+     <div>
+     {
+      chatMessages.map((chatMessage,index) => (
+      <ChatMessage key={index} name={chatMessage.name}  
+      message={chatMessage.message} />
+    ))
+    }
+     </div>
+  </div>
+  <form className="w-full p-2  border border-black flex" 
+  onSubmit={(e) => {
+    e.preventDefault();
+   dispatch(addMessage({
+    name:"sakir shaikh",
+    message:  liveMessage 
+   }))
+   setLiveMessage("")
+  }}>
+     <input className=" px-2 w-96" type="text"
+      value={liveMessage}
+       onChange={(e) => {
+      setLiveMessage(e.target.value)
+     }}/>
+     <button className="px-2 mx-2 bg-green-100">Send</button>
+     </form>
+     </>
+  );
+};
 
 export default LiveChat
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 const GOOGLE_API_KEY = "AIzaSyCNCme-U-UUPVcaEUCs90lBhBEvrxXDpr8"
 
+export const LIVE_CHAT_COUNT = 10;
+
 
 export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY; 
 
